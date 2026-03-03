ec2df2dc2c3dd3878f855e1bda9044ec882de545
danielmusselwhite <daniel.l.musselwhite@gmail.com>
2026-03-03 20:12:38 +0000

    Add - tanstack/ReactQuery to project

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './app/layout/styles.css'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -8,8 +9,12 @@ import '@fontsource/roboto/700.css';
 
 import App from './app/layout/App.tsx'
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
