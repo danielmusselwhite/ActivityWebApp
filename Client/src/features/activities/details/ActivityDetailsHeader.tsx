23f1e75f6eb46a6dadc990509134760f8f636e4d
danielmusselwhite <daniel.l.musselwhite@gmail.com>
2026-03-26 20:12:49 +0000

    Add - new map view component utilizing Leaflet API

--- Client/src/features/activities/details/ActivityDetailsHeader.tsx
+++ Client/src/features/activities/details/ActivityDetailsHeader.tsx
@@ -2,6 +2,7 @@ import { Card, Badge, CardMedia, Box, Typography, Button } from "@mui/material";
 import { act } from "react";
 import { Link } from "react-router";
 import { formatDate } from "../../../lib/util/util";
+import type { Activity } from "../../../lib/types";
 
 type Props = {
     activity: Activity
