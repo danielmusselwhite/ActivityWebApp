23369ac81c261b6ed6ac99c0a532f5bc03c36b10
Dan <56388919+danielmusselwhite@users.noreply.github.com>
2026-03-30 18:34:48 +0100

    Add Identity to project and seed users
    * Add - AspNet Identity Core to the project
    
    * Fix - Identity integration broken program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -6,6 +6,8 @@ using AutoMapper;
 using FluentValidation;
 using Application.Activities.Validators;
 using API.Middleware;
+using Domain;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -41,27 +43,42 @@ builder.Services.AddAutoMapper(cfg =>
 builder.Services.AddValidatorsFromAssemblyContaining<CreateActivityValidator>();
 builder.Services.AddTransient<ExceptionMiddleware>();
 
+
+// Adding Identity API Endpoints for User registration and login, and configuring password requirements
+builder.Services.AddIdentityApiEndpoints<User>(opt =>
+{
+    opt.User.RequireUniqueEmail = true;
+})
+.AddRoles<IdentityRole>()
+.AddEntityFrameworkStores<Persistence.AppDbContext>();
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
-// adding middleware
-app.UseMiddleware<ExceptionMiddleware>();
-
+app.UseMiddleware<ExceptionMiddleware>(); // adding middleware for API requests
 app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000")); // Allow all requests from our React App (host urls)
+
+// Adding identity API requirements for authentication and authorization
+app.UseAuthentication();
+app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
 app.MapControllers();
+app.MapGroup("api").MapIdentityApi<User>(); // Map the endpoints for the Identity API, which will handle user registration and login
 
-#region service locator pattern to get an instance of the AppDbContext and seed the database with initial data
+#region service locator pattern to get an instance of the AppDbContext & UserManager and seed the database with initial data
 // we need to create a scope to get an instance of the AppDbContext, as it is registered as a scoped service, and we are in the Program.cs file which is the entry point of the application, so we can't use dependency injection to get an instance of the AppDbContext, so we need to create a scope and then get an instance of the AppDbContext from the service provider, then we can apply any pending migrations to the database and seed the database with initial data 
 using var scope = app.Services.CreateScope(); 
 var services = scope.ServiceProvider;
 try
 {
-    // get an instance of the AppDbContext and apply any pending migrations to the database, then seed the database with initial data
+    // apply any pending migrations to the database, then seed the database with initial data
     var context = services.GetRequiredService<Persistence.AppDbContext>();
+    var userManager = services.GetRequiredService<UserManager<User>>();
     await context.Database.MigrateAsync();
     #if DEBUG
-    await Persistence.DbInitializer.SeedData(context);
+    await Persistence.DbInitializer.SeedData(context, userManager);
     #endif
 }
 catch (Exception ex)
--- Persistence/AppDbContext.cs
+++ Persistence/AppDbContext.cs
@@ -1,9 +1,11 @@
 using System;
+using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext<User>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
