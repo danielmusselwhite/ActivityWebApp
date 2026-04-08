046534ee874c7f3496c394e4097326ef2aef7b85
danielmusselwhite <daniel.l.musselwhite@gmail.com>
2026-04-08 20:16:36 +0100

    refactor(program): restructure Program.cs with regions and improved organization

--- API/Program.cs
+++ API/Program.cs
@@ -1,37 +1,37 @@
+using API.Middleware;
 using Application.Activities.Queries;
 using Application.Core;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using AutoMapper;
-using FluentValidation;
-using Application.Activities.Validators;
-using API.Middleware;
-using Domain;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc.Authorization;
 using Application.Interfaces;
+using Domain;
+using FluentValidation;
 using Infrastructure.Security;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-#region  Add services to the container.
+#region Service Configuration
+// Add services to the container.
 
-// Adding controllers with a global authorization policy, which will require all API endpoints to be accessed by authenticated users
+// Add controllers with a global authorization policy, requiring all API endpoints to be accessed by authenticated users.
 builder.Services.AddControllers(opt =>
 {
     var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
     opt.Filters.Add(new AuthorizeFilter(policy));
 });
-builder.Services.AddDbContext<Persistence.AppDbContext>( options =>
+
+// Add DbContext for Entity Framework Core.
+builder.Services.AddDbContext<Persistence.AppDbContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-#endregion
 
+// Add Cross-Origin Resource Sharing (CORS) services.
 builder.Services.AddCors();
 
-#region Adding our custom services
+// Add MediatR for handling CQRS pattern.
 builder.Services.AddMediatR(x =>
 {
     x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>();
@@ -39,32 +39,34 @@ builder.Services.AddMediatR(x =>
     x.AddOpenBehavior(typeof(ValidationBehaviour<,>));
 });
 
-
-// x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
-// builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining<GetActivityDetails.Handler>());
-#endregion
-
-builder.Services.AddScoped<IUserAccessor, UserAccessor>(); // Registering our custom service for accessing the current user, which will be used in our API endpoints to get the current user's information and perform actions on behalf of the user
-
-// Registering auto mapper
+// Add AutoMapper for object mapping.
 builder.Services.AddAutoMapper(cfg =>
 {
     cfg.AddMaps(typeof(MappingProfiles).Assembly);
 });
 
-builder.Services.AddValidatorsFromAssemblyContaining<CreateActivityValidator>();
+// Add FluentValidation for request validation.
+builder.Services.AddValidatorsFromAssemblyContaining<Application.Activities.Validators.CreateActivityValidator>();
+
+// Add custom middleware for exception handling.
 builder.Services.AddTransient<ExceptionMiddleware>();
 
+// Add custom service for accessing the current user.
+builder.Services.AddScoped<IUserAccessor, UserAccessor>();
+#endregion
 
-// Adding Identity API Endpoints for User registration and login, and configuring password requirements
+#region Identity Services
+// Add Identity API endpoints for user registration and login, and configure password requirements.
 builder.Services.AddIdentityApiEndpoints<User>(opt =>
 {
     opt.User.RequireUniqueEmail = true;
 })
 .AddRoles<IdentityRole>()
 .AddEntityFrameworkStores<Persistence.AppDbContext>();
+#endregion
 
-#region Adding authentication and authorization services
+#region Authentication & Authorization
+// Add custom authentication and authorization services.
 builder.Services.AddAuthorization(opt =>
 {
     opt.AddPolicy("IsActivityHost", policy =>
@@ -72,42 +74,51 @@ builder.Services.AddAuthorization(opt =>
         policy.AddRequirements(new IsHostRequirement());
     });
 });
-builder.Services.AddTransient<IAuthorizationHandler, IsHostRequirementHandler>(); // Registering our custom authorization handler for the "IsActivityHost" policy, which will check if the current user is the host of the activity they are trying to access
+builder.Services.AddTransient<IAuthorizationHandler, IsHostRequirementHandler>();
 
+// Add default authorization services to ensure that the [Authorize] attribute works correctly.
 builder.Services.AddAuthorization();
 #endregion
 
 var app = builder.Build();
 
-app.UseMiddleware<ExceptionMiddleware>(); // adding middleware for API requests
-app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000", "https://localhost:3000")); // Allow all requests from our React App (host urls)
+#region Application Pipeline
+// Configure the HTTP request pipeline.
 
-// Adding identity API requirements for authentication and authorization
+// Use custom middleware for API exception handling.
+app.UseMiddleware<ExceptionMiddleware>();
+
+// Use CORS to allow requests from the React app.
+app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000", "https://localhost:3000"));
+
+// Use authentication and authorization.
 app.UseAuthentication();
 app.UseAuthorization();
 
-// Configure the HTTP request pipeline.
+// Map controllers for API endpoints.
 app.MapControllers();
-app.MapGroup("api").MapIdentityApi<User>(); // Map the endpoints for the Identity API, which will handle user registration and login
 
-#region service locator pattern to get an instance of the AppDbContext & UserManager and seed the database with initial data
-// we need to create a scope to get an instance of the AppDbContext, as it is registered as a scoped service, and we are in the Program.cs file which is the entry point of the application, so we can't use dependency injection to get an instance of the AppDbContext, so we need to create a scope and then get an instance of the AppDbContext from the service provider, then we can apply any pending migrations to the database and seed the database with initial data 
-using var scope = app.Services.CreateScope(); 
+// Map Identity API endpoints for user management.
+app.MapGroup("api").MapIdentityApi<User>();
+#endregion
+
+#region Database Seeding
+// Seed the database with initial data.
+using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 try
 {
-    // apply any pending migrations to the database, then seed the database with initial data
+    // Apply any pending migrations and seed the database.
     var context = services.GetRequiredService<Persistence.AppDbContext>();
     var userManager = services.GetRequiredService<UserManager<User>>();
     await context.Database.MigrateAsync();
-    #if DEBUG
     await Persistence.DbInitializer.SeedData(context, userManager);
-    #endif
 }
 catch (Exception ex)
 {
+    // Log any errors that occur during database seeding.
     var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex, "An error occurred migrating and seeding the DB.");
+    logger.LogError(ex, "An error occurred during migration");
 }
 #endregion
 
