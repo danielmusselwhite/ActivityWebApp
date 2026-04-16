8a877df0a87b7beabb9b53117928e03b317ead4e
danielmusselwhite <daniel.l.musselwhite@gmail.com>
2026-04-16 11:44:15 +0100

    feat(activities): add crop and preview photo functionality utilising react-cropper

--- Client/src/app/shared/components/PhotoUploadWidget.tsx
+++ Client/src/app/shared/components/PhotoUploadWidget.tsx
@@ -1,11 +1,19 @@
 import { Box, Grid, Typography } from "@mui/material";
 import { useDropzone } from 'react-dropzone';
-import { useCallback } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import { CloudUpload } from "@mui/icons-material";
+import Cropper, { type ReactCropperElement } from "react-cropper";
+import "cropperjs/dist/cropper.css";
 
 export default function PhotoUploadWidget() {
+    const [files, setFiles] = useState<(File & { preview: string })[]>([]);
+    const cropperRef = useRef<ReactCropperElement>(null);
+
     const onDrop = useCallback((acceptedFiles: File[]) => {
-        console.log(acceptedFiles);
+        setFiles(acceptedFiles.map(file => Object.assign(file, {
+            preview: URL.createObjectURL(file as Blob) // create previewUrl to be used as source for cropper component
+        })))
+
     }, [])
     const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
@@ -32,10 +40,32 @@ export default function PhotoUploadWidget() {
                 </Box>
             </Grid>
             <Grid size={4}>
-                <Typography variant="overline" color="secondary">Step 2 - Resize Photo</Typography>
+                {files[0]?.preview && (
+                    <>
+                        <Typography variant="overline" color="secondary">Step 2 - Resize Photo</Typography>
+                        <Cropper
+                            src={files[0].preview}
+                            style={{ height: 300, width: "90%" }}
+                            initialAspectRatio={1}
+                            aspectRatio={1}
+                            preview='.img-preview'
+                            guides={false}
+                            viewMode={1}
+                            background={false}
+                        />
+                    </>
+                )}
             </Grid>
             <Grid size={4}>
-                <Typography variant="overline" color="secondary">Step 3 - Preview & Upload Photo</Typography>
+                {files[0]?.preview && (
+                    <>
+                        <Typography variant="overline" color="secondary">Step 3 - Preview & Upload Photo</Typography>
+
+                        <div className="img-preview" style={{ width: 300, height: 300, overflow: 'hidden' }}>
+
+                        </div>
+                    </>
+                )}
             </Grid>
 
         </Grid>
